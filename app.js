73e22a82f8bb1aebda603db05bbe36ef570441c0
Joe Gencarelli <jp.genc@gmail.com>
2022-06-05 12:28:20 -0400

    Set up starter file and finished HTML

---
+++ app.js
@@ -0,0 +1,9 @@
+// Pseudo code for match game
+// Add event listeners to each list item
+// Once a card is picked, the card must flip and hsow whats underneath
+// the user picks a second card: only two cards can be picked at a time
+// check if the two chosen cards match
+// if they do, chnage the colour and remove vent listner from both cards
+// if they dont match, flip htem back to original state
+// Have a counter so we know when the game is finished 
+// once the game is finished, reset the cards
